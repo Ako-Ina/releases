cba98a9ce8860e529d64224f588eab2e2362f4fe
DeinMathelehrer <bruno@suedhoehe25.de>
2021-10-04 18:56:13 +0200

    config: modify for sanders & adapt for arch

--- config.sh
+++ config.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export device="sanders"
 
-export device=""
-
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export official="false"
-export local_manifest_url=""
-export manifest_url=""
+export ROM="Project Materium"
+export ROM_DIR="/home/bruno/Schreibtisch/materium"
+export ROM_VERSION="v1.x"
+export ROM_VENDOR_NAME="materium"
+export local_manifest_url="https://github.com/DeinMathelehrer/manifests/raw/main/materium/sanders.xml"
+export manifest_url="https://github.com/ProjectMaterium/android"
 export rom_vendor_name=""
-export branch=""
+export branch="materium-v1"
 export bacon="bacon"
 export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="50"
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="DeinMathelehrer/releases"
 
-export timezone="UTC"
+export timezone="UTC+2"
+
+export sticker_id="CAACAgIAAxkBAAEGqrFhWw08YRIs1pKtHF-J8ZT3A_i-1wACGwADguGrJX6OWYRDwUm7IQQ"
+
+
+# Rom options
+#export MATERIUM_BUILD_TYPE="gapps"
+export official=false
+
