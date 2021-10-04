a5498b5a6988e994bae1951d6ec60323f0ab25e7
DeinMathelehrer <bruno@suedhoehe25.de>
2021-10-04 18:56:13 +0200

    config: modify for sanders & adapt for arch

--- config.sh
+++ config.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="DeinMathelehrer"
+export GITHUB_EMAIL="bruno@suedhoehe25.de"
+export GITHUB_TOKEN="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"
+export TELEGRAM_CHAT="-1001501943315"
+export TELEGRAM_TOKEN="1825686201:AAHHgsFSQn9gygSeXEjVQ0bVgNrwfiDxw_4"
 
-export device=""
+export device="sanders"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export official="false"
-export local_manifest_url=""
-export manifest_url=""
+export ROM="Project Materium"
+export ROM_DIR="/home/bruno/Schreibtisch/materium"
+export ROM_VERSION="v1.x"
+export ROM_VENDOR_NAME="materium"
+export local_manifest_url="https://github.com/DeinMathelehrer/manifests/raw/main/materium/sanders.xml"
+export manifest_url="https://github.com/ProjectMaterium/android"
 export rom_vendor_name=""
-export branch=""
+export branch="materium-v1"
 export bacon="bacon"
 export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="50"
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="DeinMathelehrer/releases"
 
-export timezone="UTC"
+export timezone="UTC+2"
+
+export sticker_id="CAACAgIAAxkBAAEGqrFhWw08YRIs1pKtHF-J8ZT3A_i-1wACGwADguGrJX6OWYRDwUm7IQQ"
+
+
+# Rom options
+#export MATERIUM_BUILD_TYPE="gapps"
+export official=false
+
